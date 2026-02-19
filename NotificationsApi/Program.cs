86661402bc547a768574fac88f2f38f5f0993799
Diogo Carlomagno <diogocarlomagno@gmail.com>
2026-02-19 11:41:12 -0300

    Centralizes database context configuration

--- NotificationsApi/Program.cs
+++ NotificationsApi/Program.cs
@@ -1,28 +1,18 @@
 using Core.Models;
 using Core.Repository;
 using Infrastructure.Repository;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NotificationsApi.Configs;
 using NotificationsApi.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json").Build();
-
-var connectionString = configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-}, ServiceLifetime.Scoped);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddHealthChecks();
 
